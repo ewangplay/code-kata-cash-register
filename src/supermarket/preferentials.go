c925f6820e974241a031179004193cb767c73a27
ewangplay <wdahui@hotmail.com>
2016-03-11 16:47:17 +0800

    set preferential

--- src/supermarket/preferentials.go
+++ src/supermarket/preferentials.go
@@ -1,5 +1,10 @@
 package supermarket
 
+const (
+    BUY_TWO_GET_ONE_FREE = 1
+    DISCOUNT_95 = 2
+)
+
 type Preferentials struct {
 }
 
@@ -7,3 +12,8 @@ func NewPreferentials() *Preferentials {
     return &Preferentials{
     }
 }
+
+func (this *Preferentials) AddPreferential(id int, priority int, goods_barcode_list []string) error {
+    return nil
+}
+
--- src/supermarket/supermarket_test.go
+++ src/supermarket/supermarket_test.go
@@ -1,37 +1,38 @@
 package supermarket
 
 import (
-    "testing"
-    "os"
+	"os"
+	"testing"
 )
 
 var t_cashregister *CashRegister
 
 func TestMain(m *testing.M) {
-    //创建超市并登记商品
-    supermarket := NewSuperMarket("没钱赚商店")
-    supermarket.RegisterGoods(NewGoods("111", "苹果", "斤", 5.5, "水果"))
-    supermarket.RegisterGoods(NewGoods("222", "可乐", "瓶", 3.0, "饮料"))
-    supermarket.RegisterGoods(NewGoods("333", "康师傅方便面", "袋", 4.5, "食品"))
+	//创建超市并登记商品
+	supermarket := NewSuperMarket("没钱赚商店")
+	supermarket.RegisterGoods(NewGoods("111", "苹果", "斤", 5.5, "水果"))
+	supermarket.RegisterGoods(NewGoods("222", "可乐", "瓶", 3.0, "饮料"))
+	supermarket.RegisterGoods(NewGoods("333", "康师傅方便面", "袋", 4.5, "食品"))
 
-    //创建优惠信息
-    preferentials := NewPreferentials()
+	//创建优惠信息
+	preferentials := NewPreferentials()
+	preferentials.AddPreferential(BUY_TWO_GET_ONE_FREE, 2, []string{"222", "333"})
+	preferentials.AddPreferential(DISCOUNT_95, 1, []string{"111", "222"})
 
-    //创建收银机并关联超市，并设置优惠信息
-    t_cashregister = NewCashRegister(supermarket)
-    t_cashregister.SetPreferentials(preferentials)
+	//创建收银机并关联超市，并设置优惠信息
+	t_cashregister = NewCashRegister(supermarket)
+	t_cashregister.SetPreferentials(preferentials)
 
-    os.Exit(m.Run())
+	os.Exit(m.Run())
 }
 
 func TestSettleGoods(t *testing.T) {
-    //添加购物车
-    shoppingcart := NewShoppingCart() 
-    shoppingcart.AddGoods("111", 3)
-    shoppingcart.AddGoods("222", 2)
-    shoppingcart.AddGoods("333", 5)
+	//添加购物车
+	shoppingcart := NewShoppingCart()
+	shoppingcart.AddGoods("111", 3)
+	shoppingcart.AddGoods("222", 2)
+	shoppingcart.AddGoods("333", 5)
 
-    //收银机结算
-    t_cashregister.SettleGoods(shoppingcart)
+	//收银机结算
+	t_cashregister.SettleGoods(shoppingcart)
 }
-
