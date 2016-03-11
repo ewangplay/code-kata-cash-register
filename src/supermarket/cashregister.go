719c1c43cac9620429d92cdcbe7611e983d45f4a
ewangplay <wdahui@hotmail.com>
2016-03-11 20:11:39 +0800

    打印买二赠一的商品信息和节省的金额

--- src/supermarket/cashregister.go
+++ src/supermarket/cashregister.go
@@ -35,18 +35,23 @@ func (this *CashRegister) SettleGoods(shoppingcart *ShoppingCart) error {
 	fmt.Printf("***<%v>购物清单***\n", this.belongs_supermarket.GetName())
 
 	var sum float32
+	var sum_save float32
+
+	var hasBuyTwoGetOneFreeGoods bool
+	strBuyTwoGetOneFreeGoods := "----------------------\n买二赠一商品：\n"
 
 	for goods_barcode, goods_num := range goods_list {
 		goods := this.belongs_supermarket.QueryGoods(goods_barcode)
 
 		if goods != nil {
 
-            goods_preferential_id := NO_PREFERENTIAL
+			goods_preferential_id := NO_PREFERENTIAL
 			if this.preferentials != nil {
 				goods_preferential_id = this.preferentials.QueryGoodsPreferential(goods_barcode)
 			}
 
 			var amount float32
+			var amount_save float32
 
 			switch goods_preferential_id {
 			case NO_PREFERENTIAL:
@@ -54,22 +59,37 @@ func (this *CashRegister) SettleGoods(shoppingcart *ShoppingCart) error {
 				fmt.Printf("名称: %v, 数量：%v%v, 单价：%v(元), 小计: %v(元)\n", goods.Name, goods_num, goods.QuantityUnit, goods.UnitPrice, amount)
 
 			case BUY_TWO_GET_ONE_FREE:
-                free_num := int(goods_num / 3)
-				amount = float32(goods_num - free_num) * goods.UnitPrice
+				free_num := int(goods_num / 3)
+				amount = float32(goods_num-free_num) * goods.UnitPrice
+				amount_save = float32(free_num) * goods.UnitPrice
 				fmt.Printf("名称: %v, 数量：%v%v, 单价：%v(元), 小计: %v(元)\n", goods.Name, goods_num, goods.QuantityUnit, goods.UnitPrice, amount)
+
+				hasBuyTwoGetOneFreeGoods = true
+				strBuyTwoGetOneFreeGoods += fmt.Sprintf("名称: %v, 数量: %v%v\n", goods.Name, free_num, goods.QuantityUnit)
 
 			case DISCOUNT_95:
 				amount = float32(goods_num) * goods.UnitPrice * 0.95
+				amount_save = float32(goods_num) * goods.UnitPrice * 0.05
 				fmt.Printf("名称: %v, 数量：%v%v, 单价：%v(元), 小计: %v(元)\n", goods.Name, goods_num, goods.QuantityUnit, goods.UnitPrice, amount)
 
 			}
 
 			sum += amount
+			sum_save += amount_save
 		}
 	}
 
-	fmt.Println("----------------------")
-	fmt.Printf("总计：%v(元)\n", sum)
+	if hasBuyTwoGetOneFreeGoods {
+		fmt.Print(strBuyTwoGetOneFreeGoods)
+	}
+
+	if sum > 0 {
+		fmt.Println("----------------------")
+		fmt.Printf("总计：%v(元)\n", sum)
+	}
+	if sum_save > 0 {
+		fmt.Printf("节省：%v(元)\n", sum_save)
+	}
 	fmt.Println("**********************")
 
 	return nil
