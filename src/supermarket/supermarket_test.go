0289ce579584ae44a89fe98bfabf96fffc4dfc54
ewangplay <wdahui@hotmail.com>
2016-03-11 20:28:01 +0800

    增加测试场景

--- src/supermarket/supermarket_test.go
+++ src/supermarket/supermarket_test.go
@@ -10,14 +10,16 @@ var t_cashregister *CashRegister
 func TestMain(m *testing.M) {
 	//创建超市并登记商品
 	supermarket := NewSuperMarket("没钱赚商店")
-	supermarket.RegisterGoods(NewGoods("111", "苹果", "斤", 5.5, "水果"))
-	supermarket.RegisterGoods(NewGoods("222", "可乐", "瓶", 3.0, "饮料"))
-	supermarket.RegisterGoods(NewGoods("333", "康师傅方便面", "袋", 4.5, "食品"))
+	supermarket.RegisterGoods(NewGoods("ITEM000001", "苹果", "斤", 5.5, "水果"))
+	supermarket.RegisterGoods(NewGoods("ITEM000002", "可口可乐", "瓶", 3.0, "饮料"))
+	supermarket.RegisterGoods(NewGoods("ITEM000003", "康师傅方便面", "袋", 4.5, "食品"))
+	supermarket.RegisterGoods(NewGoods("ITEM000004", "羽毛球", "个", 1.5, "体育用品"))
+	supermarket.RegisterGoods(NewGoods("ITEM000005", "西瓜", "斤", 6, "水果"))
 
 	//创建优惠信息
 	preferentials := NewPreferentials()
-	preferentials.AddPreferential(BUY_TWO_GET_ONE_FREE, 2, []string{"222", "333"})
-	preferentials.AddPreferential(DISCOUNT_95, 1, []string{"111", "222"})
+	preferentials.AddPreferential(BUY_TWO_GET_ONE_FREE, 2, []string{"ITEM000004"})
+	preferentials.AddPreferential(DISCOUNT_95, 1, []string{"ITEM000001"})
 
 	//创建收银机并关联超市，并设置优惠信息
 	t_cashregister = NewCashRegister(supermarket)
@@ -26,12 +28,53 @@ func TestMain(m *testing.M) {
 	os.Exit(m.Run())
 }
 
-func TestSettleGoods(t *testing.T) {
+func TestNotBuyGoods(t *testing.T) {
 	//添加购物车
 	shoppingcart := NewShoppingCart()
-	shoppingcart.AddGoods("111", 3)
-	shoppingcart.AddGoods("222", 2)
-	shoppingcart.AddGoods("333", 5)
+
+	//收银机结算
+	t_cashregister.SettleGoods(shoppingcart)
+}
+
+func TestNoPreferentialGoods(t *testing.T) {
+	//添加购物车
+	shoppingcart := NewShoppingCart()
+	shoppingcart.AddGoods("ITEM000002", 3)
+	shoppingcart.AddGoods("ITEM000003", 2)
+	shoppingcart.AddGoods("ITEM000005", 1)
+
+	//收银机结算
+	t_cashregister.SettleGoods(shoppingcart)
+}
+
+func TestBuyTwoGetOneFreePreferentialGoods(t *testing.T) {
+	//添加购物车
+	shoppingcart := NewShoppingCart()
+	shoppingcart.AddGoods("ITEM000002", 3)
+	shoppingcart.AddGoods("ITEM000003", 2)
+	shoppingcart.AddGoods("ITEM000004", 5)
+
+	//收银机结算
+	t_cashregister.SettleGoods(shoppingcart)
+}
+
+func TestDiscount95PreferentialGoods(t *testing.T) {
+	//添加购物车
+	shoppingcart := NewShoppingCart()
+	shoppingcart.AddGoods("ITEM000001", 3)
+	shoppingcart.AddGoods("ITEM000003", 3)
+	shoppingcart.AddGoods("ITEM000005", 1)
+
+	//收银机结算
+	t_cashregister.SettleGoods(shoppingcart)
+}
+
+func TestBuyTwoGetOneFreeAndDiscount95PreferentialGoods(t *testing.T) {
+	//添加购物车
+	shoppingcart := NewShoppingCart()
+	shoppingcart.AddGoods("ITEM000001", 3)
+	shoppingcart.AddGoods("ITEM000002", 3)
+	shoppingcart.AddGoods("ITEM000004", 5)
 
 	//收银机结算
 	t_cashregister.SettleGoods(shoppingcart)
